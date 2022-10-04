c465f61b69717d6863d698d4b452084d2fed0904
Eliu Rosario <eliurosario@gmail.com>
2022-10-04 19:40:55 -0400

    Created array for the Upper case options

--- script.js
+++ script.js
@@ -28,6 +28,11 @@ var addNSpecialCharacters = window.confirm("Do you want your password to include
 var numbersOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var symbolsOptions = ["!", "@", "#", "$", "%", "&", "*"]
 var lettersList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var lettersUpper = []
+
+for (var i = 0; i < lettersList.length; i++) {
+  lettersUpper[i] = lettersList[i].toUppercase()
+}
 
 }
 
