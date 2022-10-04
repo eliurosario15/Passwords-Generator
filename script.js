11dc2b85b48e65bcc9c679ce5072c452fea1e56a
Eliu Rosario <eliurosario@gmail.com>
2022-10-04 19:48:45 -0400

    Created IF statements for all password criteria's

--- script.js
+++ script.js
@@ -23,16 +23,36 @@ if (passwordLenght < 8 || passwordLenght > 128) {
 var addNumbers = window.confirm("Do you want your password to include numbers?")
 var addUppercase = window.confirm("Do you want your password to include lowercase letters?")
 var addLowercase = window.confirm("Do you want your password to include uppercase letters?")
-var addNSpecialCharacters = window.confirm("Do you want your password to include special characters?")
+var addSpecialCharacters = window.confirm("Do you want your password to include special characters?")
 
 var numbersOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var symbolsOptions = ["!", "@", "#", "$", "%", "&", "*"]
 var lettersList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var lettersUpper = []
 
+var allOptions = []
+
+
 for (var i = 0; i < lettersList.length; i++) {
   lettersUpper[i] = lettersList[i].toUppercase()
 }
+
+if (addNumbers === true) {
+  allOptions.push(addNumbers)
+}
+
+if (addUppercase === true) {
+  allOptions.push(lettersUpper)
+}
+
+if (addLowercase === true) {
+  allOptions.push(addLowercase)
+}
+
+if (addSpecialCharacters === true) {
+  allOptions.push(addSpecialCharacters)
+}
+
 
 }
 
