4b96ef97632fc4d4bd4740effbd03965e56f8561
Eliu Rosario <eliurosario@gmail.com>
2022-10-04 19:55:15 -0400

    Added array for upper case letters and deleted function to modify lower case array

--- script.js
+++ script.js
@@ -28,14 +28,9 @@ var addSpecialCharacters = window.confirm("Do you want your password to include 
 var numbersOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var symbolsOptions = ["!", "@", "#", "$", "%", "&", "*"]
 var lettersList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var lettersUpper = []
+var lettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 var allOptions = []
-
-
-for (var i = 0; i < lettersList.length; i++) {
-  lettersUpper[i] = lettersList[i].toUppercase()
-}
 
 if (addNumbers === true) {
   allOptions.push(addNumbers)
@@ -52,6 +47,8 @@ if (addLowercase === true) {
 if (addSpecialCharacters === true) {
   allOptions.push(addSpecialCharacters)
 }
+
+console.log(allOptions)
 
 
 }
